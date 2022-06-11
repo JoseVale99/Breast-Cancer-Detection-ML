05acda82bf72667701e17a1db9aba6dfeddad8cd
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2022-06-10 22:08:37 -0500

    ✅ Adding: config pagination

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 app.config['PAGE_SIZE'] = 10
-app.config['VISIBLE_PAGE_COUNT'] = 10
+app.config['VISIBLE_PAGE_COUNT'] = 4
 app.config.from_object('config')
 
 
