5f9c2ec86acbcf097fdc9f2c5d0a4c62d4b4f188
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2022-06-11 15:46:41 -0500

    ✅ Adding: secret key app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['PAGE_SIZE'] = 10
 app.config['VISIBLE_PAGE_COUNT'] = 4
 app.config.from_object('config')
-
+app.config['UPLOAD_FOLDER'] =  os.path.join('static')
 
 from .views import routes
 
