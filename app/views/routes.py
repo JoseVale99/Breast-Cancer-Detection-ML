446f03708d5e4519708140b8b174823655d5f87f
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2022-06-11 15:45:33 -0500

    🐛 FIX: routes images and notifications

--- app/views/routes.py
+++ app/views/routes.py
@@ -6,22 +6,41 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from flask import render_template, request, current_app
+from flask import (redirect, render_template, request, current_app,flash, Blueprint, url_for)
 from .flask_pager import Pager
+from io import BytesIO
+import os
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# upload file
+@app.route('/upload_file', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if request.files['file'].filename == '':
+            flash('¡El archivo no se cargo correctamente!','error')
+            return redirect(url_for('index'))
+
+        f = request.files['file']
+        f.save(f.filename)
+        df  =  pd.read_csv(f.filename)
+        image = os.path.join(current_app.config['UPLOAD_FOLDER'], 'plot1.png')
+
+        flash('¡Archivo cargado exitosamente!','success')
+        return render_template('index.html',image=image)
+    
+    
+
+# model 1
 @app.route('/Regresionlogistic')
 def model_logistic():
     df=pd.read_csv ("data/data.csv")
-    df.isna ().sum()
-    df=df.drop ('Unnamed: 32',axis=1)
 
+    df=df.drop ('Unnamed: 32',axis=1)
     df ['diagnosis'].unique()
-    
     df['diagnosis'].value_counts()
     df ['diagnosis'] = df ['diagnosis'].map ({'M':1,'B':0})
     df ['diagnosis'].unique()
@@ -46,9 +65,6 @@ def model_logistic():
     data  = list(zip (id,y_pred))
 
     page = int(request.args.get('page', 1))
-
-    # count = 300
-    # data_1 = range(count)
 
     pager = Pager(page, len(data))
     pages = pager.get_pages()
