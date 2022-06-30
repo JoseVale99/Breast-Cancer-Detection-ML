068e791b050902d6c7ec38a09b4798bf0e003ba0
MarkosCe <cesar.markos99@gmail.com>
2022-06-30 16:35:24 -0500

    Modificiacion de los templates: grafica agregada

--- app/views/routes.py
+++ app/views/routes.py
@@ -1,18 +1,21 @@
-from pyparsing import removeQuotes
 from app import app
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from flask import (redirect, render_template, url_for,request, current_app,flash)
-from .flask_pager import Pager
 import os
-import io
-import base64
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import svm
+from sklearn.svm import SVC
+
+import numpy as np
+import json
+import plotly
+import plotly.express as px
+
+global prelog
 
 @app.route('/')
 def index():
@@ -33,234 +36,137 @@ def upload_file():
         image_2 = os.path.join(current_app.config['UPLOAD_FOLDER'], 'plot2.jpg')
 
         flash('¡Archivo cargado exitosamente!','success')
-        return render_template('index.html',plot_1= plot_1, plot_2=image_2)
-    
-    
+        return render_template('index.html',plot_1= plot_1, plot_2=image_2)  
 
-# model 1
+# model 1 - LogisticRgression
 @app.route('/Regresionlogistic')
 def model_logistic():
-    x_test,x_train,y_train,y_test =  process_data()
+    x_train, x_test, y_train, y_test =  process_data()
 
-    lr = LogisticRegression().fit(x_train,y_train)
-    y_pred = lr.predict (x_test)
-    
-    id = [i for i in range (1,len(y_pred)+1)]
+    lr = LogisticRegression()
+    lr.fit(x_train,y_train)
+    y_pred = lr.predict(x_test)
 
-    data  = list(zip (id,y_pred))
+    ##Datos de tabla
+    data_to_show, id = table_data(y_pred=y_pred, y_test=y_test)
 
-    page = int(request.args.get('page', 1))
+    ###grafica
+    df = chart_data(y_test=y_test, y_pred=y_pred, id=id)
+    fig = px.line(df, x='id', y='valor', color='tipo', markers=True)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    pager = Pager(page, len(data))
-    pages = pager.get_pages()
+    #tasa de precision
+    pred = accuracy_score(y_test,y_pred)
 
-    offset = (page - 1) * current_app.config['PAGE_SIZE']
-    limit = current_app.config['PAGE_SIZE']
-    data_to_show = data[offset: offset + limit]  
+    return render_template('logistic.html', precision=pred, data=data_to_show, graphJSON=graphJSON)
 
-    pred = round(accuracy_score(y_test,y_pred)*100)
-
-    return render_template('logistic.html', pages=pages, pred=pred,data=data_to_show)
-
+#model 2 - DecisionTreeClasifier
 @app.route('/DecisionTreeClassifier')
 def model_DecisionTreeClassifier():
-    x_test,x_train,y_train,y_test =  process_data()
+    x_train, x_test, y_train, y_test =  process_data()
 
-    # Decision Tree Classifier
     dtc = DecisionTreeClassifier()
-    #Loading the training data in the model
-    dtc.fit (x_train,y_train)
+    dtc.fit(x_train,y_train)
+    y_pred = dtc.predict(x_test)
 
-    #Predicting output with the test data
-    y_pred = dtc.predict (x_test)
-    id = [i for i in range (1,len(y_pred)+1)]
+    #datos tabla
+    data_to_show, id = table_data(y_pred=y_pred, y_test=y_test)
 
-    data  = list(zip (id,y_pred))
+    ###grafica
+    df = chart_data(y_test=y_test, y_pred=y_pred, id=id)
+    fig = px.line(df, x='id', y='valor', color='tipo', markers=True)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    page = int(request.args.get('page', 1))
+    #tasa de precision
+    pred = accuracy_score(y_test,y_pred)
 
-    pager = Pager(page, len(data))
-    pages = pager.get_pages()
+    return render_template('DecisionTreeClassifier.html', precision=pred, data=data_to_show, graphJSON=graphJSON)
 
-    offset = (page - 1) * current_app.config['PAGE_SIZE']
-    limit = current_app.config['PAGE_SIZE']
-    data_to_show = data[offset: offset + limit]
-
-    pred = round(accuracy_score(y_test,y_pred)*100)
-
-    return render_template('DecisionTreeClassifier.html',data=data_to_show,pred=pred ,pages=pages)
-
+#model 3 - RandomForestClassifier
 @app.route('/RandomForestClassifier')
 def model_RandomForestClassifier():
-    x_test,x_train,y_train,y_test =  process_data()
+    x_train, x_test, y_train, y_test =  process_data()
 
     rfc = RandomForestClassifier()
-    #Loading the training data in the model
-    rfc.fit (x_train,y_train)
-    #Predicting output with test data
-    y_pred = rfc.predict (x_test)
-    id = [i for i in range (1,len(y_pred)+1)]
+    rfc.fit(x_train,y_train)
+    y_pred = rfc.predict(x_test)
 
-    data  = list(zip (id,y_pred))
+    #datos tabla
+    data_to_show, id = table_data(y_pred=y_pred, y_test=y_test)
 
-    page = int(request.args.get('page', 1))
+    ###grafica
+    df = chart_data(y_test=y_test, y_pred=y_pred, id=id)
+    fig = px.line(df, x='id', y='valor', color='tipo', markers=True)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    pager = Pager(page, len(data))
-    pages = pager.get_pages()
+    #tasa de precision
+    pred = accuracy_score(y_test,y_pred)
 
-    offset = (page - 1) * current_app.config['PAGE_SIZE']
-    limit = current_app.config['PAGE_SIZE']
-    data_to_show = data[offset: offset + limit]
+    return render_template('RandomForestClassifier.html', precision=pred, data=data_to_show, graphJSON=graphJSON)
 
-    pred = round(accuracy_score(y_test,y_pred)*100)
-
-    return render_template('RandomForestClassifier.html',data=data_to_show,pred=pred ,pages=pages)
-
+#model 4 - supportVectorClasifier
 @app.route('/Supportvectorclassifier')
 def model_support_vector_classifier():
+    x_train, x_test, y_train, y_test =  process_data()
 
-    x_test,x_train,y_train,y_test =  process_data()
-
-    svc = svm.SVC ()
-    #Loading the training data in the model
-    svc.fit (x_train,y_train)
-
-    #Predicting output with test data
+    svc = SVC()
+    svc.fit(x_train,y_train)
     y_pred = svc.predict (x_test)
+
+    #datos tabla
+    data_to_show, id = table_data(y_pred=y_pred, y_test=y_test)
+
+    ###grafica
+    df = chart_data(y_test=y_test, y_pred=y_pred, id=id)
+    fig = px.line(df, x='id', y='valor', color='tipo', markers=True)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    #tasa de precision
+    pred = accuracy_score(y_test,y_pred)
+
+    return render_template('Supportvectorclassifier.html', precision=pred, data=data_to_show, graphJSON=graphJSON)
+
+#-------->>
+#datos para mostrar en la tabla
+def table_data(y_pred, y_test):
     id = [i for i in range (1,len(y_pred)+1)]
+    data  = list(zip (id,y_test, y_pred))
+    data_to_show = data
+    return data_to_show, id
 
-    data  = list(zip (id,y_pred))
+#funcion para crear dataframe
+def chart_data(y_test, y_pred, id):
+    array = []
+    for i in range(len(y_test)):
+        array.append('Prediccion')
 
-    page = int(request.args.get('page', 1))
+    for i in range(len(y_pred)):
+        array.append('Real')
 
-    pager = Pager(page, len(data))
-    pages = pager.get_pages()
-
-    offset = (page - 1) * current_app.config['PAGE_SIZE']
-    limit = current_app.config['PAGE_SIZE']
-    data_to_show = data[offset: offset + limit]
-
-    pred = round(accuracy_score(y_test,y_pred)*100)
-
-    return render_template('Supportvectorclassifier.html',data=data_to_show,pred=pred ,pages=pages)
-
+    y_testn = pd.array(y_test)
+    valores = np.concatenate((y_testn, y_pred), axis=0)
+    ids = np.concatenate((id,id), axis=0)
+    df = pd.DataFrame({'tipo':array,
+                        'id':ids,
+                        'valor':valores})
+    return df
 
 def process_data():
+    #Leer data.csv
     df = pd.read_csv('data.csv')
+    #limpiar datos
+    df=df.drop('Unnamed: 32',axis=1)
+    df['diagnosis'] = df['diagnosis'].map({'M':1,'B':0})
 
-    df=df.drop ('Unnamed: 32',axis=1)
-    #df ['diagnosis'].unique()
-    #df['diagnosis'].value_counts()
-    df ['diagnosis'] = df['diagnosis'].map ({'M':1,'B':0})
-    #df ['diagnosis'].unique()
-
+    #dividir datos para el modelo
     x = df.drop('diagnosis', axis=1)
     y = df['diagnosis']
-    from sklearn.model_selection import train_test_split
+
     x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.25)
 
-    from sklearn.preprocessing import StandardScaler
+    #transformar datos
     ss = StandardScaler()
     x_train = ss.fit_transform(x_train)
     x_test = ss.fit_transform(x_test)
 
-    from sklearn.linear_model import LogisticRegression
-    logr = LogisticRegression()
-    logr.fit(x_train, y_train)
-    y_pred = logr.predict(x_test)
-
-    ##Datos de tabla
-    id = [i for i in range (1,len(y_pred)+1)]
-    data  = list(zip (id,y_test, y_pred))
-    page = int(request.args.get('page', 1))
-    pager = Pager(page, len(data))
-    pages = pager.get_pages()
-    offset = (page - 1) * current_app.config['PAGE_SIZE']
-    limit = current_app.config['PAGE_SIZE']
-    data_to_show = data[offset: offset + limit]
-    ###
-
-    from sklearn.metrics import accuracy_score
-    precision = accuracy_score(y_test, y_pred)
-
-    image = os.path.join(current_app.config['UPLOAD_FOLDER'], 'plotlog.png')
-
-    '''plot = sns.countplot(y_pred)
-
-    x_1 = [1,3,5,7,9,10]
-    y_1 = [6,9,12,15,18,21]
-    x_2 = [-6,-4,-2,0,2,3]
-    y_2 = [-1,2,5,8,11,14]
-
-    ids = [1]
-    for i in range(2, 172):
-        ids.append(i)
-
-    y_test = pd.array(y_test)
-
-    plt.clf()
-    img = io.BytesIO()
-    plt.plot(ids,y_test,marker='.',color='darkred',linestyle='-')
-    plt.plot(ids,y_pred,marker='.',color='darkblue',linestyle='-')
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()   
-    return render_template('logistic.html', imagen={ 'imagen': plot_url })'''
-
-    
-
-
-    '''X = df.iloc[:, 2:31].values
-    Y = df.iloc[:, 1].values
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
-    from sklearn.preprocessing import StandardScaler
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.fit_transform(X_test)
-
-    from sklearn.linear_model import LogisticRegression
-    log = LogisticRegression(random_state=0)
-    log.fit(X_train, Y_train)
-    precision = log.score(X_train, Y_train)
-
-    from sklearn.metrics import confusion_matrix
-    cm = confusion_matrix(Y_test, log.predict(X_test))
-    TN = cm[0][0]
-    TP = cm[1][1]
-    FN = cm[1][0]
-    FP = cm[0][1]
-
-    precisionm = (TP + TN)/(TP + TN + FN + FP)'''
-    
-    '''x = df.drop ('diagnosis',axis=1)
-    y = df ['diagnosis']
-    
-    
-    x_train,x_test,y_train,y_test = train_test_split (x,y,test_size=0.3)
-    x_train.shape
-    y_train.shape
-
-    ss = StandardScaler()
-    x_train = ss.fit_transform (x_train)
-    x_test = ss.fit_transform (x_test)
-
-    lr = LogisticRegression().fit(x_train,y_train)
-    y_pred = lr.predict (x_test)
-    
-    id = [i for i in range (1,len(y_pred)+1)]
-
-    data  = list(zip (id,y_pred))
-
-    page = int(request.args.get('page', 1))
-
-    pager = Pager(page, len(data))
-    pages = pager.get_pages()
-
-    offset = (page - 1) * current_app.config['PAGE_SIZE']
-    limit = current_app.config['PAGE_SIZE']
-    data_to_show = data[offset: offset + limit]'''    
-
-    #return render_template('logistic.html', pages=pages, data=data_to_show)
-    return render_template('logistic.html', precision=precision, imagen=image, pages=pages, data=data_to_show)
-    return (x_test,x_train,y_train,y_test)
+    return(x_train, x_test, y_train, y_test)
